e9f7f456c4eaf7a518b50c2bfbb09f2fc1012400
OkayAyHumus <97745736+OkayAyHumus@users.noreply.github.com>
2022-04-21 22:08:55 +0900

    Update settings.py

--- crsproject/settings.py
+++ crsproject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'f*48*j)6uddwwayd@wcu+k*=e&_@67@_*ndq$e4rvv4r-yz)5@'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
